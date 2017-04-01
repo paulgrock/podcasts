28879c7c08336b1ee1c119ea9f72105d27e79114
Paul Grock <paulgrock@gmail.com>
2017-04-01 08:38:50 -0700

    Switch to async/await just because

--- server.js
+++ server.js
@@ -11,20 +11,21 @@ app.use(bodyParser.urlencoded({
 
 app.set('port', process.env.SERVER_PORT || 3001);
 
+async function fetchItunesJsonAndParse(url) {
+	const fetchP = await fetch(url)
+	const json = fetchP.json();
+	return json;
+}
+
 app.get('/api/search', (req, res) => {
-	fetch(`https://itunes.apple.com/search?media=podcast&entity=podcast&term=${req.query.q}`).
-		then((r) => r.json()).
-		then((searchResults) => res.json(searchResults)).
+	fetchItunesJsonAndParse(`https://itunes.apple.com/search?media=podcast&entity=podcast&term=${req.query.q}`).
+		then((results) => res.json(results)).
 		catch(e => console.error(e));
 })
 
 app.get('/api/lookup', (req, res) => {
-	fetch(`https://itunes.apple.com/lookup?id=${req.query.id}&country=us&media=podcast&entity=podcastEpisode&limit=5`).
-		then((r) => r.json()).
-		then((searchResults) => {
-			console.log(searchResults);
-			return res.json(searchResults)
-		}).
+	fetchItunesJsonAndParse(`https://itunes.apple.com/lookup?id=${req.query.id}&country=us&media=podcast&entity=podcastEpisode&limit=5`).
+		then((searchResults) => res.json(searchResults)).
 		catch(e => console.error(e));
 })
 
